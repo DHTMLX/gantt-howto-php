5914e66eb7524f13a569baf5a0f8c094cbb5e494
AlexKlimenkov <shurick.klimenkov@gmail.com>
2018-09-19 00:01:25 +0300

    [fix] typo in addLink function

--- src/gantt.php
+++ src/gantt.php
@@ -137,7 +137,7 @@ function deleteTask($request, $response, $args) {
 }
 
 // create new link
-function addLlink($request, $response, $args) {
+function addLink($request, $response, $args) {
 	$link = getLink($request->getParsedBody());
 	$db = getConnection();
 	$query = "INSERT INTO gantt_links(source, target, type) VALUES (:source,:target,:type)";
